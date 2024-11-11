d52b7707dcdd593f332fdaf874c6a77761839b83
Pi Lanningham <pi.lanningham@gmail.com>
2024-11-11 10:33:13 -0500

    Switch to modularized builds, to support both web and dedicated servers

--- src/d_loop.c
+++ src/d_loop.c
@@ -45,7 +45,11 @@
 
 uint32_t instanceUID;
 EM_JS(void, hydra_set_ip, (uint32_t ip), {
-    window.HydraMultiplayer.setIP(ip);
+    let g = typeof window !== 'undefined' ? window : global;
+    let hydra = !!g ? g.HydraMultiplayer : null;
+    if (!!hydra) {
+        window.HydraMultiplayer.setIP(ip);
+    }
 })
 
 // The complete set of data for a particular tic.
