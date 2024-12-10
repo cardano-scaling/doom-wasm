a363552750242290acd5ee6c788d858cb397a76c
Pi Lanningham <pi.lanningham@gmail.com>
2024-12-10 00:15:31 -0500

    Fix kills buffer bug

--- src/doom/hydra.h
+++ src/doom/hydra.h
@@ -13,11 +13,11 @@
 
 EM_ASYNC_JS(void, hydra_send_packet, (uint32_t to, uint32_t from, uint32_t *kills, size_t kills_len, char *packet, size_t len), {
     let data = HEAPU8.subarray(packet, packet + len);
-    let kills = HEAPU8.subarray(kills, kills + kills_len);
+    let killsBuffer = HEAPU8.subarray(kills, kills + kills_len);
     let g = typeof window !== "undefined" ? window : global;
     let hydra = !!g ? g.HydraMultiplayer : null;
     if (!!hydra) {
-        await hydra.SendPacket(to, from, kills, data);
+        await hydra.SendPacket(to, from, killsBuffer, data);
     }
 })
 
