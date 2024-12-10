c8fbd92116b030de32bb0ac52a9addc71abc89dc
Pi Lanningham <pi.lanningham@gmail.com>
2024-12-10 02:08:35 -0500

    Fix addressing

--- src/doom/hydra.h
+++ src/doom/hydra.h
@@ -13,7 +13,7 @@
 
 EM_ASYNC_JS(void, hydra_send_packet, (uint32_t to, uint32_t from, int *kills, size_t kills_len, char *packet, size_t len), {
     let data = HEAPU8.subarray(packet, packet + len);
-    let killsBuffer = HEAP32.subarray(kills, kills + kills_len);
+    let killsBuffer = HEAP32.subarray(kills/4, kills/4 + kills_len);
     let g = typeof window !== "undefined" ? window : global;
     let hydra = !!g ? g.HydraMultiplayer : null;
     if (!!hydra) {
